3d39ca37d70dc8d73516a0e2117cf6b17896bf10
Chris Cox <cox.chris@me.com>
2026-02-18 15:18:26 -0800

    Initial Elixir project scaffold
    Add initial project skeleton for the AshMultiAccount library: create mix.exs (Elixir ~> 1.19), lib/AshMultiAccount with a hello/0 function and doctest, ExUnit test and test helper, .formatter.exs, .gitignore, and a README placeholder. No dependencies are configured yet; this commit lays out the basic structure for further development.

---
+++ lib/ash_multi_account.ex
@@ -0,0 +1,18 @@
+defmodule AshMultiAccount do
+  @moduledoc """
+  Documentation for `AshMultiAccount`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshMultiAccount.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_multi_account_test.exs
@@ -0,0 +1,8 @@
+defmodule AshMultiAccountTest do
+  use ExUnit.Case
+  doctest AshMultiAccount
+
+  test "greets the world" do
+    assert AshMultiAccount.hello() == :world
+  end
+end
